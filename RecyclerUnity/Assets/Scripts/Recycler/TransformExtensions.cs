6d29c824cac8363243834ea05cebe66e08ae22fa
surmwill <william.surmak@gmail.com>
2024-05-20 15:52:04 -0600

    adding extension methods

---
+++ RecyclerUnity/Assets/Scripts/Recycler/TransformExtensions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Extension methods for Transforms
+/// </summary>
+public static class TransformExtensions
+{
+    /// <summary>
+    /// Returns the children of a transform as an IEnumerable
+    /// </summary>
+    public static IEnumerable<Transform> Children(this Transform t)
+    {
+        return t.Cast<Transform>();
+    }
+}
