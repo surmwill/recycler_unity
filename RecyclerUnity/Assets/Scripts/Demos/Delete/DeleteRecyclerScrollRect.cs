3c046378d14c7bd4cfd5148b8e6b9438523419a7
surmwill <william.surmak@gmail.com>
2024-06-02 12:57:56 -0600

    starting delete demo

--- RecyclerUnity/Assets/Scripts/Demos/Delete/DeleteRecyclerScrollRect.cs
+++ RecyclerUnity/Assets/Scripts/Demos/Delete/DeleteRecyclerScrollRect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InsertAndResizeRecycler : RecyclerScrollRect<InsertAndResizeData>
+public class DeleteRecyclerScrollRect : RecyclerScrollRect<object>
 {
     
 }
