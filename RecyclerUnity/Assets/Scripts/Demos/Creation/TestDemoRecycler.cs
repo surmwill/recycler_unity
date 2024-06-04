68161f2669c000967c4d4fd209fb9b42fcfb033c
surmwill <william.surmak@gmail.com>
2024-06-04 11:09:46 -0600

    fixing up some comments

--- RecyclerUnity/Assets/Scripts/Demos/Creation/TestDemoRecycler.cs
+++ RecyclerUnity/Assets/Scripts/Demos/Creation/TestDemoRecycler.cs
@@ -11,50 +11,19 @@ public class TestDemoRecycler : MonoBehaviour
     
     private static readonly string[] Words =
     { 
-        "hold",
-        "work",
-        "wore",
-        "days",
-        "meat",
-        "hill",
-        "club",
-        "boom",
-        "tone",
-        "grey",
-        "bowl",
-        "bell",
-        "kick",
-        "hope",
-        "over",
-        "year",
-        "camp",
-        "tell",
-        "main",
-        "lose",
-        "earn",
-        "name",
-        "hang",
-        "bear",
-        "heat",
-        "trip",
-        "calm",
-        "pace",
-        "home",
-        "bank",
-        "cell",
-        "lake",
-        "fall",
-        "fear",
-        "mood",
-        "head",
-        "male",
-        "evil",
-        "toll",
-        "base"
+        "hold", "work", "wore", "days", "meat",
+        "hill", "club", "boom", "tone", "grey",
+        "bowl", "bell", "kick", "hope", "over",
+        "year", "camp", "tell", "main", "lose",
+        "earn", "name", "hang", "bear", "heat",
+        "trip", "calm", "pace", "home", "bank",
+        "cell", "lake", "fall", "fear", "mood",
+        "head", "male", "evil", "toll", "base"
     };
 
     private void Start()
     {
+        // Create data containing the words from the array, each with a random background color
         DemoRecyclerData[] entryData = new DemoRecyclerData[Words.Length];
         for (int i = 0; i < Words.Length; i++)
         {
