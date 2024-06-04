66de90fb32bd94eae74d9d474afa3da5cec0577c
surmwill <william.surmak@gmail.com>
2024-06-04 09:15:05 -0600

    starting creation demo

--- RecyclerUnity/Assets/Scripts/Demos/Creation/DemoRecyclerEntry.cs
+++ RecyclerUnity/Assets/Scripts/Demos/Creation/DemoRecyclerEntry.cs
@@ -4,27 +4,34 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CreationRecyclerScrollRectEntry : RecyclerScrollRectEntry<DemoRecyclerData>
+public class DemoRecyclerEntry : RecyclerScrollRectEntry<DemoRecyclerData>
 {
     [SerializeField]
-    private TMP_Text _word = null;
+    private TMP_Text _wordText = null;
 
     [SerializeField]
-    private TMP_Text _index = null;
+    private TMP_Text _indexText = null;
 
     [SerializeField]
     private Image _background = null;
     
+    // Called when this entry is bound to new data
     protected override void OnBindNewData(DemoRecyclerData entryData)
     {
+        _wordText.text = entryData.Word;
+        _background.color = entryData.BackgroundColor;
         
+        _indexText.text = Index.ToString();
     }
 
+    // Called when this entry is bound with data it had before (and therefore still currently has)
     protected override void OnRebindExistingData()
     {
-
+        Debug.Log(Data.Word);
+        Debug.Log(Data.BackgroundColor);
     }
 
+    // Called when this entry has been sent back to the recycling pool
     protected override void OnSentToRecycling()
     {
        
