ec446446d3ac8ace58af4e0f9f8972922e69a23d
surmwill <william.surmak@gmail.com>
2024-06-09 16:06:41 -0600

    adding collider creation to validate

---
+++ RecyclerUnity/Assets/Scripts/Recycler/RecyclerScrollRectEndcapState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The states and endcap can be in
+/// </summary>
+public enum RecyclerScrollRectEndcapState
+{
+    InPool = 0,
+    InCache = 1,
+    Visible = 2,
+}
