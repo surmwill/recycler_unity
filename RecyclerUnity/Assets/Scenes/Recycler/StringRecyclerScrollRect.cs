dd14894c39180f531e7b00e04100cbd8a31805c8
surmwill <william.surmak@gmail.com>
2024-06-13 17:51:46 -0600

    cleaning up

--- RecyclerUnity/Assets/Scenes/Recycler/StringRecyclerScrollRect.cs
+++
@@ -1,7 +0,0 @@
-
-/// <summary>
-/// A recycler displaying a list of strings (used for testing the recycler with simple data)
-/// </summary>
-public class StringRecyclerScrollRect : RecyclerScrollRect<StringRecyclerData, string>
-{
-}
