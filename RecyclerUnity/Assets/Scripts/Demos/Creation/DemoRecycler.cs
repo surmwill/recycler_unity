09c8fb2b27a3950903820f621e338e8c18d81a58
surmwill <william.surmak@gmail.com>
2024-06-13 17:14:55 -0600

    wip adding keys

--- RecyclerUnity/Assets/Scripts/Demos/Creation/DemoRecycler.cs
+++ RecyclerUnity/Assets/Scripts/Demos/Creation/DemoRecycler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DemoRecycler : RecyclerScrollRect<DemoRecyclerData>
+public class DemoRecycler : RecyclerScrollRect<DemoRecyclerData, string>
 {
   
 }
--- RecyclerUnity/Assets/Scripts/Demos/ScrollToIndex/ScrollToIndexRecyclerScrollRect.cs
+++ RecyclerUnity/Assets/Scripts/Demos/ScrollToIndex/ScrollToIndexRecyclerScrollRect.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 /// <summary>
 /// Recycler to demo scrolling to an index
 /// </summary>
-public class ScrollToIndexRecyclerScrollRect : RecyclerScrollRect<ScrollToIndexData>
+public class ScrollToIndexRecyclerScrollRect : RecyclerScrollRect<ScrollToIndexData, string>
 {
 }
