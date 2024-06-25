55cab8314f47cee863b5bfbc6937aa98044b840b
surmwill <william.surmak@gmail.com>
2024-06-25 13:11:59 -0600

    making demo a little prettier

--- RecyclerUnity/Assets/Scripts/Demos/AutoSize/AutoSizeEntry.cs
+++ RecyclerUnity/Assets/Scripts/Demos/AutoSize/AutoSizeEntry.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+
 /// <summary>
 /// Recycler entry to test if we can handle auto-sized content
 /// </summary>
@@ -17,7 +15,7 @@ public class AutoSizeEntry : RecyclerScrollRectEntry<AutoSizeData, string>
 
     protected override void OnBindNewData(AutoSizeData entryData)
     {
-        _titleText.text = $"Randomly Generated {entryData.NumLines} Line(s)";
+        _titleText.text = $"Randomly Generated <color=red>{entryData.NumLines}</color> Line(s)";
 
         _linesText.text = Enumerable.Range(0, entryData.NumLines)
             .Aggregate(string.Empty, (s, i) => s + $"Line {i + 1}\n")
