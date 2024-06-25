6274180e1ec46aca21a091d5b45e9a3a3f73f1b0
surmwill <william.surmak@gmail.com>
2024-06-25 10:57:20 -0600

    adding endcap to creation demo

---
+++ RecyclerUnity/Assets/Scripts/Demos/Creation/DemoEndcap.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoEndcap : RecyclerScrollRectEndcap<EmptyRecyclerData, string>
+{
+    public override void OnFetchedFromRecycling()
+    {
+        
+    }
+
+    public override void OnSentToRecycling()
+    {
+       
+    }
+}
