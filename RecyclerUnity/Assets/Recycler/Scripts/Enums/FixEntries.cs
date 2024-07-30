2aee962ae6e844682b62fa586f4a821fdabcc9b9
surmwill <william.surmak@gmail.com>
2024-07-30 19:03:57 -0400

    commenting enums

--- RecyclerUnity/Assets/Recycler/Scripts/Enums/FixEntries.cs
+++ RecyclerUnity/Assets/Recycler/Scripts/Enums/FixEntries.cs
@@ -2,13 +2,14 @@
 namespace RecyclerScrollRect
 {
     /// <summary>
-    /// When we insert or remove an entry or modify its size, and it's onscreen, other entries will need to get pushed around to accomodate its size change.
-    /// This defines how the entries should get pushed around on a size change.
+    /// If we're updating the size of a visible entry, then we'll either be pushing other entries or creating extra space for other entries to occupy.
+    /// This defines how and what entries will get moved. If we're not updating an entry in the visible window, this is ignored,
+    /// and the parameter will be overriden with whatever value only moves other offscreen entries, preserving the view of what's on-screen.
     /// </summary>
     public enum FixEntries
     {
-        Below = 0, // All entries below the modified entry will stay unmoved 
-        Above = 1, // All entries above the modified entry will stay unmoved 
-        Mid = 2, // The modified entry will push the entries above and below it equally
+        Below = 0,      // All entries below the one modified will stay unmoved. 
+        Above = 1,      // All entries above the one modified will stay unmoved. 
+        Mid = 2,        // All entries above and below the one modified will be moved equally.
     }
 }
--- RecyclerUnity/Assets/Recycler/Scripts/Enums/ScrollToAlignment.cs
+++ RecyclerUnity/Assets/Recycler/Scripts/Enums/ScrollToAlignment.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace RecyclerScrollRect
 {
     /// <summary>
     /// Defines the position within an entry to center on when we scroll to it.
-    /// For example, we can scroll to an entry's middle or its top edge.
     /// </summary>
     public enum ScrollToAlignment
     {
-        EntryMiddle = 0, // Center on the middle of the entry
-        EntryBottom = 1, // Center on the bottom edge of the entry
-        EntryTop = 2, // Center on the top edge of the entry
+        EntryMiddle = 0,    // Center on the middle of the entry.
+        EntryBottom = 1,    // Center on the bottom edge of the entry.
+        EntryTop = 2,       // Center on the top edge of the entry.
     }
 }
